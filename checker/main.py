7e835e4707d129418b790e5e38a3f4e0bfc51e41
H <38281581+MHuiG@users.noreply.github.com>
2025-05-28 16:23:59 +0800

    Update main.py

--- checker/main.py
+++ checker/main.py
@@ -34,8 +34,8 @@ def get_issue_id(a):
         t=a[i]
         #判断值是否存在在序列b中
         if "#" in t:
-            return t
-    return "0#0"
+            return int(t[1:])
+    return 0
 
 def load_config():
     f = open('config.yml', 'r',encoding='utf-8')
